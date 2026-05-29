4fd7c303122b66a0203a0882bab51139861fd998
anacorrea791 <correaanaclara@gmail.com.br>
2026-05-29 07:49:29 -0300

    finalizando projeto

--- obj/Debug/net10.0/NeuroVestAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/NeuroVestAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeuroVestAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021cbfc31b8a5829c54c2dc58dfe0201270dd6f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327b1ba51e47ec5b18528a806665bd227337031a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeuroVestAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeuroVestAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
